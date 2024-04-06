3ebf532b85e2f5adffc6f2fc5626623202ebc051
Subhajitorrin <orrin2op@gmail.com>
2024-04-06 17:10:36 +0530

    update

--- src/API/GetVideosBySearch.js
+++ src/API/GetVideosBySearch.js
@@ -4,7 +4,7 @@ import APIKEY from "../API/APIKEY";
 // Function to make GET request
 export const GetVideosBySearch = (query) => {
     const encodedQuery = encodeURIComponent(query);
-    const url = `https://www.googleapis.com/youtube/v3/search?key=${APIKEY}&q=${encodedQuery}&type=video&maxResults=1&part=snippet`
+    const url = `https://www.googleapis.com/youtube/v3/search?key=${APIKEY}&q=${encodedQuery}&type=video&maxResults=5&part=snippet`
     return axios.get(url)
         .then(response => {
             return response.data;
