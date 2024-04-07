65b824e93e6f2a1dc55eeacecf163eb72197c506
Subhajitorrin <orrin2op@gmail.com>
2024-04-07 16:09:21 +0530

    iframe details

--- src/Components/IframeDetails.jsx
+++ src/Components/IframeDetails.jsx
@@ -9,6 +9,50 @@ import { RiDownloadLine } from "react-icons/ri";
 import { RiMenuAddFill } from "react-icons/ri";
 import { BsThreeDots } from "react-icons/bs";
 
+
+function formatNumber(num) {
+  if (num >= 1000000000) {
+    return (num / 1000000000).toFixed(1) + "B";
+  }
+  if (num >= 1000000) {
+    return (num / 1000000).toFixed(1) + "M";
+  }
+  if (num >= 1000) {
+    return (num / 1000).toFixed(1) + "K";
+  }
+  return num;
+}
+
+function timeAgo(timestamp) {
+  const currentDate = new Date();
+  const pastDate = new Date(timestamp);
+  const timeDifference = currentDate.getTime() - pastDate.getTime();
+
+  const seconds = Math.floor(timeDifference / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+  const weeks = Math.floor(days / 7);
+  const months = Math.floor(days / 30);
+  const years = Math.floor(days / 365);
+
+  if (years > 0) {
+    return years === 1 ? "1 year ago" : years + " years ago";
+  } else if (months > 0) {
+    return months === 1 ? "1 month ago" : months + " months ago";
+  } else if (weeks > 0) {
+    return weeks === 1 ? "1 week ago" : weeks + " weeks ago";
+  } else if (days > 0) {
+    return days === 1 ? "1 day ago" : days + " days ago";
+  } else if (hours > 0) {
+    return hours === 1 ? "1 hour ago" : hours + " hours ago";
+  } else if (minutes > 0) {
+    return minutes === 1 ? "1 minute ago" : minutes + " minutes ago";
+  } else {
+    return "Just now";
+  }
+}
+
 function IframeDetails({ id }) {
   const [videoDetails, setVideoDetails] = useState([]);
   useEffect(() => {
@@ -18,19 +62,19 @@ function IframeDetails({ id }) {
     setVideoDetails(iframedata);
   }, []);
 
-  // useEffect(() => {
-  //   console.log(videoDetails[0].snippet.title);
-  // }, [videoDetails]);
+  useEffect(() => {
+    console.log();
+  }, [videoDetails]);
 
   return (
     <div className="ifreamdetailscontainer">
       <div className="titleContainer">
-        <h3>Titile</h3>
+        <h3>{videoDetails[0].snippet.title}</h3>
       </div>
       <div className="channelContainer">
         <div className="channelleft">
           <div>
-            <h5 className="channelname">Channel Name</h5>
+            <h5 className="channelname">{videoDetails[0].snippet.channelTitle}</h5>
             <p className="subscount">2.32k subscribers</p>
           </div>
           <div className="subscribe">
@@ -40,27 +84,27 @@ function IframeDetails({ id }) {
         <div className="channelright">
           <div className="likeNdislike">
             <div className="like">
-              <BiLike />
-              <p>15K</p>
+              <BiLike className="likeicon" />
+              <p>{formatNumber(videoDetails[0].statistics.likeCount)}</p>
             </div>
             <div className="verticalline"></div>
             <div className="dislike">
-              <BiDislike />
+              <BiDislike className="likeicon" />
             </div>
           </div>
           <div className="share">
-            <PiShareFat />
+            <PiShareFat className="likeicon" />
             <h5>Share</h5>
           </div>
           <div className="download">
-            <RiDownloadLine />
+            <RiDownloadLine className="likeicon" />
             <h5>Download</h5>
           </div>
           <div className="save">
-            <RiMenuAddFill />
+            <RiMenuAddFill className="likeicon" />
             <h5>Save</h5>
           </div>
-          <div className="menu">
+          <div className="menuiframe">
             <BsThreeDots />
           </div>
         </div>
